2d409fbfed34ffc954d99f0ff6f071eb736efc85
Abdul Domado <abdulm.domado@exist.com>
2023-10-26 17:32:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,11 @@ import pickle
 import pandas as pd
 
 # Model deployment
-from flask import Flask
 import streamlit as st
 
 #model = pickle.load(open('gb_tk.pkl', 'rb'))
 
-st.title("Eskwealabs S3")
+st.title("Eskwelabs S3")
 
 #adding a selectbox
 choice = st.text_input(
