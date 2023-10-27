544a1d8efcd4a79e773449fd055425b0717d0e5a
Abdul Domado <abdulm.domado@exist.com>
2023-10-27 23:04:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,41 @@
-# General Libraries
 import pandas as pd
-
-# Model deployment
 import streamlit as st
+from sklearn.metrics.pairwise import euclidean_distances
 
 recommender_pool_df = pd.read_csv('spotify_s3g3_rec_pool.csv')
 recommender_pool_df = recommender_pool_df.sort_values('track_name')
 recommender_pool_df['track_names_with_artist'] = recommender_pool_df['track_name'] + ' by ' + recommender_pool_df['artist_name']
 recommender_pool_df = recommender_pool_df.drop_duplicates(subset=['track_names_with_artist'])
 options = recommender_pool_df['track_names_with_artist'].to_list()
+feature_cols = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness','liveness', 'valence', 'tempo']
 
-st.title("OPM Genre & Tracks Recommender")
+st.title("OPM Recommender Engine")
 st.caption("by: Team Swifthrees")
 st.image("https://img.freepik.com/free-vector/musical-pentagram-sound-waves-notes-background_1017-33911.jpg?w=1800&t=st=1698402872~exp=1698403472~hmac=5c67bc8bbf36f092b5ffb32a69f8ac13d10d9e76c53dc69b88f87b0aacb7cee5")
 st.divider()
 
 choice = st.selectbox(
-    "Select Track Name: ", options = options)
+    "Select Seed Track: ", options = options)
+
+def get_euclidean_dist(x,y):
+	euclidean_dist =  euclidean_distances(x.values.reshape(1, -1), y.values.reshape(1, -1)).flatten()[0]
+	return euclidean_dist
 
 if st.button("Submit", type="primary"):
-    track = recommender_pool_df.loc[recommender_pool_df['track_names_with_artist'] == choice]
-    predicted_genre = track['predicted_genre'].values[0]
-    recommended_tracks = recommender_pool_df[(recommender_pool_df['predicted_genre'] == predicted_genre) & (recommender_pool_df['track_names_with_artist'] != choice) & (recommender_pool_df['predicted_genre_proba'] >= 0.5)].sort_values('predicted_genre_proba', ascending=False)
-    recommended_tracks_sample = recommended_tracks.sample(n=10)
+    seed_track = recommender_pool_df.loc[recommender_pool_df['track_names_with_artist'] == choice].iloc[0]
+    recommender_pool_df['euclidean_dist_features'] = recommender_pool_df.apply(lambda x: get_euclidean_dist(x[feature_cols],seed_track[feature_cols]), axis=1)
+    predicted_genre = seed_track['predicted_genre']
+    recommended_tracks = recommender_pool_df[(recommender_pool_df['predicted_genre'] == predicted_genre) & (recommender_pool_df['track_names_with_artist'] != choice)].sort_values('euclidean_dist_features')
 
     st.divider()
     st.markdown("<h5>Predicted Genre: </h5>", unsafe_allow_html = True)
     st.info(predicted_genre.upper())
     st.markdown("<br><h5>Similar Songs: </h5>", unsafe_allow_html = True)
-    for track in recommended_tracks_sample['track_names_with_artist'][:10]:
-    	st.info("\t🎵 " + track)
+
+    max_euclidean = recommended_tracks['euclidean_dist_features'].max() 
+    min_euclidean = recommended_tracks['euclidean_dist_features'].min()
+    recommended_tracks['euclidean_scaled'] =  (1 - (recommended_tracks['euclidean_dist_features'] - min_euclidean) / (max_euclidean - min_euclidean)).abs()
+
+    for i, track in recommended_tracks.iterrows():
+    	#st.info("\t🎵 " + track['track_names_with_artist'])
+    	st.progress(value = track['euclidean_scaled'], text= "\t🎵 " + track['track_names_with_artist'])
