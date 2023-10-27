cdc498782ac11f8d04f2ba8cae382acb0e0a8feb
Abdul Domado <abdulm.domado@exist.com>
2023-10-27 19:36:13 +0800

    Update streamlit_app.py and dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,31 @@
 # General Libraries
-import pickle
 import pandas as pd
 
 # Model deployment
 import streamlit as st
 
-#model = pickle.load(open('gb_tk.pkl', 'rb'))
+recommender_pool_df = pd.read_csv('spotify_s3g3_rec_pool.csv')
+recommender_pool_df = recommender_pool_df.sort_values('track_name')
+recommender_pool_df['track_names_with_artist'] = recommender_pool_df['track_name'] + ' by ' + recommender_pool_df['artist_name']
+recommender_pool_df = recommender_pool_df.drop_duplicates(subset=['track_names_with_artist'])
+options = recommender_pool_df['track_names_with_artist'].to_list()
 
-st.title("Eskwelabs S3 Recommender Project")
+st.title("OPM Genre & Tracks Recommender")
 st.caption("by: Team Swifthrees")
-
+st.image("https://img.freepik.com/free-vector/musical-pentagram-sound-waves-notes-background_1017-33911.jpg?w=1800&t=st=1698402872~exp=1698403472~hmac=5c67bc8bbf36f092b5ffb32a69f8ac13d10d9e76c53dc69b88f87b0aacb7cee5")
 st.divider()
-#adding a selectbox
-choice = st.text_input(
-    "Enter Input:")
 
-st.button("Submit", type="primary")
+choice = st.selectbox(
+    "Select Track Name: ", options = options)
+
+if st.button("Submit", type="primary"):
+    track = recommender_pool_df.loc[recommender_pool_df['track_names_with_artist'] == choice]
+    predicted_genre = track['predicted_genre'].values[0]
+    recommended_tracks = recommender_pool_df[(recommender_pool_df['predicted_genre'] == predicted_genre) & (recommender_pool_df['track_names_with_artist'] != choice)].sort_values('predicted_genre_proba', ascending=False)
+    
+    st.divider()
+    st.markdown("<h5>Predicted Genre: </h5>", unsafe_allow_html = True)
+    st.info(predicted_genre.upper())
+    st.markdown("<br><h5>Top 10 Similar Songs: </h5>", unsafe_allow_html = True)
+    for track in recommended_tracks['track_names_with_artist'][:10]:
+    	st.info("\t🎵 " + track)
